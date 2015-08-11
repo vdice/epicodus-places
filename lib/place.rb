bee1a296d1508f1d3d672be7aa803b4bead97053
Jing Wait & Vaughn Dice <student@epicodus.com>
2015-08-11 11:32:25 -0700

    add name save .all .clear specs and pall all

--- lib/place.rb
+++ lib/place.rb
@@ -1,8 +1,23 @@
 class Place
+  @@place = []
+
   define_method(:initialize) do |name|
     @name = name
   end
+
   define_method(:name) do
     @name
+  end
+
+  define_method(:save) do
+    @@place.push(self)
+  end
+
+  define_singleton_method(:all) do
+    @@place
+  end
+
+  define_singleton_method(:clear) do
+    @@place = []
   end
 end
--- spec/place_spec.rb
+++ spec/place_spec.rb
@@ -2,10 +2,38 @@ require("rspec")
 require("place")
 
 describe(Place) do
+  before() do
+    Place.clear()
+  end
+  
   describe("#name") do
     it("lets you enter a place you have been to") do
       test_place = Place.new("portland")
       expect(test_place.name()).to(eq("portland"))
     end
+  end
+
+  describe("#save") do
+    it("lets you add a new place to the places you have been to list") do
+      test_place = Place.new("portland")
+      test_place.save()
+      expect(Place.all()).to(eq([test_place]))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Place.all()).to(eq([]))
+    end
+
+  end
+
+  describe(".clear") do
+    it("clear out all of the saved places") do
+      Place.new("portland").save()
+      Place.clear()
+      expect(Place.all()).to(eq([]))
+    end
+
   end
 end
